efd93044ff5463a4dddff517d9e19f99584636a3
Kinga Wigurska-Bąk <kinga.wigurskabak@wipro.com>
2019-04-17 13:33:54 +0200

    Parse data from form

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,13 @@ import axios from 'axios';
 
 import { API_URL } from '../config/config';
 
+const preparedSurveyData = data => {
+  return Object.entries(data).map(item => ({
+    questionId: item[0],
+    answer: item[1]
+  }));
+};
+
 export const fetchQuestions = async function() {
   const questions = await axios.get(`${API_URL}/getQuestions`);
 
@@ -9,7 +16,11 @@ export const fetchQuestions = async function() {
 };
 
 export const saveSurvey = async function(data) {
-  const questions = await axios.post(`${API_URL}/saveSurvey`, data);
+  const parsedData = {
+    ...data,
+    answers: preparedSurveyData(data.answers)
+  };
+  const questions = await axios.post(`${API_URL}/saveSurvey`, parsedData);
 
   return questions;
 };
