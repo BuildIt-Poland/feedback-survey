fc71404770ea3975dc4eabdf5de11af9408fc956
Kinga Wigurska-Bąk <kinga.wigurskabak@wipro.com>
2019-05-07 10:07:15 +0200

    Update tests

--- src/components/App/__tests__/Main.test.js
+++ src/components/App/__tests__/Main.test.js
@@ -3,6 +3,10 @@ import { create } from 'react-test-renderer';
 
 import Main from '../Main';
 
+jest.mock('react-router-dom', () => ({
+  Link: 'Link'
+}));
+
 describe('COMPONENT - App Main', () => {
   it('renders correctly', () => {
     const component = create(<Main />);
--- src/components/Header/__tests__/Header.test.js
+++ src/components/Header/__tests__/Header.test.js
@@ -3,6 +3,10 @@ import { create } from 'react-test-renderer';
 
 import Header from '../Header';
 
+jest.mock('react-router-dom', () => ({
+  Link: 'Link'
+}));
+
 describe('COMPONENT - Header', () => {
   it('renders correctly', () => {
     const component = create(<Header />);
