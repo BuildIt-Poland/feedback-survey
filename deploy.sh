8de6926c8ef46ed8f404944bea9a285242af1f2f
Kinga Wigurska-Bąk <kinga.wigurskabak@wipro.com>
2019-05-22 11:20:28 +0200

    Add deploy to travis

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+npm install
+
+REACT_APP_API_URL=DEV_API_URL npm run build
+
+# Install the AWS CLI so we can publish to S3
+# --upgrade option tells to upgrade any requirements that are already installed.
+# --user option tells to install the program to a subdirectory of your user directory to avoid modifying
+# libraries used by your operating system.
+pip install awscli --upgrade --user
+
+# Sync our build folder with our S3 bucket
+# --acl public-read says deploy the files with public read access
+# --delete says to delete files in the bucket that aren't present in the build folder
+aws s3 sync build/ "s3://feedback-survey-web-dev" --acl public-read --delete
