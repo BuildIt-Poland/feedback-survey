b9e875382648e1eead4f434f0206b1c3bf78ce2f
Kinga Wigurska-Bąk <kinga.wigurskabak@wipro.com>
2019-05-31 09:24:30 +0200

    Change copy on Welcome Page

--- src/components/WelcomePage/WelcomePage.js
+++ src/components/WelcomePage/WelcomePage.js
@@ -23,7 +23,7 @@ const WelcomePage = ({ match = { params: {} } }) => {
     <Main bgImage="/image/TextBalloons_illustration.png">
       <Page>
         <StyledHeading level="1" margin={{ bottom: 'none', top: 'none' }} color={colorRed}>
-          Performance Snapshot on RSR Consulting Talent - Consultant:
+          Performance Snapshot on Consulting Talent - Consultant:
         </StyledHeading>
         <StyledHeading level="2" margin={{ top: spacingSmall, bottom: spacingSmall }} color={colorRed}>
           {employeeName}
